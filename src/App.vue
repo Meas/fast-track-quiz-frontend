2381ed726dca28b1cfa3df85d296089edb05e0cc
FedjaD <durmicfedja@gmail.com>
2019-04-03 16:29:35 +0200

    installed vuetifyjs, transition for questions

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Quiz App</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-toolbar>
+    <v-container>
+      <v-content>
+        <quiz />
+      </v-content>
+    </v-container>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Quiz from './components/Quiz'
 
 export default {
-  name: 'app',
+  name: 'App',
   components: {
-    HelloWorld
+    Quiz
+  },
+  data () {
+    return {
+      //
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Quiz.vue
@@ -0,0 +1,65 @@
+<template>
+    <div>
+      <div>
+        <v-btn v-on:click="next">Next</v-btn>
+      </div>
+      <transition name="list-complete" mode="out-in">
+        <!-- <span v-for="(index, item) in items" v-bind:key="item" v-if="index == activeIndex" class="list-complete-item">
+          {{ item }}
+        </span> -->
+        <div :key="activeIndex" class="list-complete-item">
+          {{activeQuestion}}
+        </div>
+      </transition>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Quiz',
+  data() {
+    return {
+      items: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+      activeIndex: 0
+    }
+  },
+  computed: {
+    activeQuestion() {
+      return this.items[this.activeIndex];
+    }
+  },
+  methods: {
+    next() {
+      this.activeIndex++;
+    }
+    /* randomIndex: function () {
+      return Math.floor(Math.random() * this.items.length)
+    },
+    add: function () {
+      this.items.splice(this.randomIndex(), 0, this.nextNum++)
+    },
+    remove: function () {
+      this.items.splice(this.randomIndex(), 1)
+    }, */
+  }
+}
+</script>
+<style>
+.list-complete-item {
+  transition: all 1s;
+  display: inline-block;
+  position: absolute;
+}
+.list-complete-enter
+/* .list-complete-leave-active below version 2.1.8 */ {
+  opacity: 0;
+  transform: translateX(30px);
+}
+.list-complete-leave-to {
+  opacity: 0;
+  transform: translateX(-30px);
+}
+.list-complete-leave-active {
+  position: absolute;
+}
+</style>
